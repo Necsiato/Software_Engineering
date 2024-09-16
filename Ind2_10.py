9ae838d2dc8eb9447724e2d461875b69a06bfeff
Denis Klenze <disbalancertram@gmail.com>
2024-09-17 03:51:37 +0500

    add .py and pic/

---
+++ Ind2_10.py
@@ -0,0 +1,3 @@
+#В исходной строке заменить все пробелы на нижнее подчеркивание (_)
+sentence = " Hello world "
+print(sentence.replace(' ', '_'))
---
+++ Ind2_5.py
@@ -0,0 +1,2 @@
+day, mouth, year = 12, "Декабрь" ,2022
+print(f"Сегодня {day} {mouth} {year}.", end=" Всего хорошего!")
