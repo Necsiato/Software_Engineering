bdc830a45312b4ec606ebbb6cb26b1baf6d09216
Denis Klenze <disbalancertram@gmail.com>
2024-11-13 05:04:04 +0500

    FIX

--- Ind8_1.py
+++
@@ -1,9 +0,0 @@
-#Создание класса
-class Food:
-    #Определение конструктора
-    def __init__(self, name, calorie, price):
-        self.name = name
-        self.calorie = calorie
-        self.price = price
-#Создание экземпляра класса
-milk = Food("milk",58, 100)
--- Lab8_1.py
+++
@@ -1,9 +0,0 @@
-#Создание класса
-class Car:
-    #Определение конструктора
-    def __init__(self, make, model):
-        #Определение атрибутов
-        self.make = make
-        self.model = model
-#Создание экземпляра класса
-my_car = Car("Toyota", "Corolla")
